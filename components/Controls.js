254e8ad02ecfd4282bb01872b8facd1505d83eaf
Christian <christianreynoso96@gmail.com>
2019-10-20 13:23:48 +0800

    created hidden buttons on top of image

--- components/Controls.js
+++ components/Controls.js
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Button, Image, View, StyleSheet } from 'react-native'
+import { TouchableOpacity, ImageBackground, View, StyleSheet, Text } from 'react-native'
 
 export default Controls = (props) => {
+    const handleClick = (direction) => {
+        console.log(direction)
+    }
+
     return (
         <View style={styles.container}>
-            <Image style={styles.image} source={require('../static/images/controls.png')} />
-        </View>
+
+            <ImageBackground style={styles.image} source={require('../static/images/controls.png')}>
+                <View style={styles.buttonsContainer}>
+
+                    <View style={styles.topButtons}>
+                        <TouchableOpacity style={styles.buttonStyle} onPress={() => handleClick('up')} />
+                    </View>
+
+                    <View style={styles.midButtons}>
+                        <TouchableOpacity style={styles.buttonStyle} onPress={() => handleClick('left')} />
+                        <TouchableOpacity style={styles.buttonStyle} onPress={() => handleClick('right')} />
+                    </View>
+
+                    <View style={styles.botButtons}>
+                        <TouchableOpacity style={styles.buttonStyle} onPress={() => handleClick('down')} />
+                    </View>
+
+                </View>
+            </ImageBackground>
+
+        </View >
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        // borderWidth: 2,
+        // borderColor: 'blue',
         height: '50%',
         width: '50%',
         flexDirection: 'column',
@@ -19,16 +44,36 @@ const styles = StyleSheet.create({
     },
 
     image: {
-        borderWidth: 2,
-        borderColor: 'black',
-        // position: 'absolute',
         height: '100%',
         width: '100%',
         aspectRatio: 1,
         resizeMode: 'stretch',
     },
 
-    hiddenButton: {
+    buttonsContainer: {
+        flex: 1,
+        justifyContent: 'space-around'
+    },
 
-    }
+    buttonStyle: {
+        // borderWidth: 2,
+        // borderColor: 'red',
+        width: 50,
+        height: 50,
+    },
+
+    topButtons: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignSelf: 'center'
+    },
+    midButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    botButtons: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignSelf: 'center'
+    },
 })
